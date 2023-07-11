d4c7dc49cfc0d0cd9b50795c35ba92285406a496
hanzadahannie <hanzadaakylbekovna@mail.com>
2023-07-11 16:29:34 +0600

    hw2

---
+++ script.js
@@ -0,0 +1,68 @@
+// let a='hello'
+// let b='Ivan'
+// let c=a[0] . toUpperCase()
+// let d=a. slice(1)
+// let e=b[0] .toLowerCase()
+// let f=b.slice(1)
+// console.log(c+d+' '+e+f)
+
+// let a='lorem ipsum dolor'
+// let b=a.length
+// console.log(b)
+
+// let a = 'lorem ipsum dolor sit amet'
+// вернуть sit lorem ipsum dolor amet
+// let b= a.slice(a.lastIndexOf('s'),a.lastIndexOf('s')+3 )
+// console.log(b)
+// let c=a.slice(0,a.lastIndexOf('r')+1)
+// console.log(c)
+// let d=a.slice(a.lastIndexOf('a'))
+// console.log(d)
+// console.log(b+' '+c+' '+d)
+
+// let a = 'children'
+//верунть CHILDREn
+// let b=a.slice(0,7).toUpperCase()
+ // let c=a.slice(-1).toLowerCase()
+
+ // console.log(b+c)
+
+ // let a = 'boss'
+// верунть bsso,
+// let b=a.slice(2)
+// console.log(a[0]+b+a[1])
+
+ // let a = 'sunday'
+// вернуть index d
+// console.log(a[3])
+
+// let a = 'user'
+// let b = 'new'
+// let c=a.slice(0,2)
+// верунть new us
+// console.log(b+' '+c)
+
+ // let a = 'monday'
+ // let b = 'day'
+// let c=a.slice(0,3)
+// вернуть daydaymon
+// console.log(b+b+c)
+
+// let a = 'HELLO'
+// вернуть 'hElLo'
+// console.log(a[0].toLowerCase()+a[1].toUpperCase()+a[2].toLowerCase()+a[3].toUpperCase()+a[4].toLowerCase())
+
+ // let a = 'monkey'
+ // let b = 'banana'
+// вернуть manak
+// console.log(a[0]+b.slice(1,4)+a[3])
+
+
+ // let a = 'hello'
+// let b=a.replace('ll','ks')
+// console.log(b)
+
+ let a = 5
+ let b = '400'
+// вернуть '450'
+console.log(b.replace('0',a))
